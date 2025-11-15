a439a5c05da8fbe1912aaefe3cb77b20d46b350c
Ferb2004 <96085883+Ferb2004@users.noreply.github.com>
2025-11-15 02:24:48 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -5,16 +5,14 @@ from dotenv import load_dotenv
 import os
 import asyncio
 
-#Ładuje plik z kluczami i je definiuje
+
 load_dotenv(".env")
 TOKEN: str = os.getenv("TOKEN")
 
 
-#Definiowanie bota, prefixu komend i uprawnień
 bot = commands.Bot(command_prefix="$", intents=discord.Intents.all())
 
 
-#Powiadomienie o gotowości bota i statusie komend
 @bot.event
 async def on_ready():
     print("Bot gotowy!")
@@ -25,19 +23,16 @@ async def on_ready():
         print("Problem z synchoronizacją komend:", e)
 
 
-#Funkcja ładująca pliki z komendami
 async def load():
     for filename in os.listdir("./cogs"):
         if filename.endswith(".py"):
             await bot.load_extension(f"cogs.{filename[:-3]}")
 
 
-#Funkcja załączająca funkcję ładującą pliki z komendami i włączająca bota
 async def main():
     async with bot:
         await load()
         await bot.start(TOKEN)
 
 
-#Załącza główną funkcje
 asyncio.run(main())
