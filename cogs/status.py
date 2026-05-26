7b7660dfe4f4ceff69b1346fe7e1ae9916fe324e
Ferb2004 <96085883+Ferb2004@users.noreply.github.com>
2026-05-26 03:21:56 +0200

    Add files via upload

--- cogs/status.py
+++ cogs/status.py
@@ -8,6 +8,7 @@ import traceback
 import os
 from dotenv import load_dotenv
 from mcstatus import JavaServer
+import aiohttp
 
 logger = get_logger(__name__)
 
@@ -21,52 +22,67 @@ server = None
 class Status(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.latest_version = None
 
     @commands.Cog.listener()
     async def on_ready(self):
         log_cog_loaded(__name__)
 
         if ipSerwera is None:
-            logger.info("Nie wpisano serwera, status nie będzie pokazywany.")
-
-        global server
-        server = JavaServer.lookup(f"{ipSerwera}:{portSerwera}")
-        self.statusUpdate.start()
+            logger.info("Nie wpisano adresu serwera minecraft.")
+        else:
+            global server
+            server = JavaServer.lookup(f"{ipSerwera}:{portSerwera}")
+        self.get_latest_version.start()
+        self.status_update.start()
 
     def cog_unload(self):
-        self.statusUpdate.cancel()
+        self.get_latest_version.cancel()
+        self.status_update.cancel()
 
     async def on_app_command_error(self, interaction, error):
         logger.error("[SlashCommand] Błąd komendy:")
         traceback.print_exception(type(error), error, error.__traceback__)
 
-    @tasks.loop(minutes=5)
-    async def statusUpdate(self):
+    @tasks.loop(minutes=30)
+    async def get_latest_version(self):
+        url = "https://api.github.com/repos/Ferb2004/kaczogrod-discord-bot/releases/latest"
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url, headers={"Accept": "application/vnd.github+json"}) as r:
+                if r.status == 200:
+                    data = await r.json()
+                    self.latest_version = data["tag_name"]
 
-        status = server.status(tries=5)
-        gracze = status.players.online
-        if gracze == 0:
+
+    @tasks.loop(minutes=5)
+    async def status_update(self):
+        gracze: int | None = None
+        if server is not None:
+            status = server.status(tries=5)
+            gracze = status.players.online
+        if gracze == 0 or gracze is None:
             if version is not None:
-                status = f"{version}" #✅
+                if version == self.latest_version:
+                    status = f"✅{version}"
+                else:
+                    status = f"ℹ️{version} | Dostępna aktualizacja."
             else:
                 status = f"🔨Własna kompilacja"
         else:
-            if gracze == 1:
-                status = f"{gracze} gracz na serwerze {ipSerwera}"
-            else:
-                status = f"{gracze} graczy na serwerze {ipSerwera}"
+            status = f"{gracze} gracz{'y' if gracze != 1 else ''} na serwerze {ipSerwera}"
+
 
         await self.bot.change_presence(
             activity=discord.CustomActivity(name=status)
         )
-        logger.success("Zaktualizowana status")
+        logger.success(f"Zaktualizowana status na {status}")
 
 
-    @statusUpdate.before_loop
+    @status_update.before_loop
     async def before_status_update(self):
         await self.bot.wait_until_ready()
 
-    @statusUpdate.error
+    @status_update.error
     async def update_online_count_error(self, error):
         logger.error(f"Błąd w statusUpdate: {error}", exc_info=error)
 
